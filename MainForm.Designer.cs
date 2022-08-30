80a1d3fd1a65036a12a2cc0f34cc8ee56c90fa23
MidoxMAX <18028414+MidoxMAX@users.noreply.github.com>
2022-08-30 21:20:41 +0800

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace 按字幕截取音频
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSelectSub = new System.Windows.Forms.Button();
+            this.BtnSelectVideo = new System.Windows.Forms.Button();
+            this.LBFiles = new System.Windows.Forms.ListBox();
+            this.LBSubCount = new System.Windows.Forms.Label();
+            this.BtnSplit = new System.Windows.Forms.Button();
+            this.BtnGetAudio = new System.Windows.Forms.Button();
+            this.TxtFileIndex = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtSubTimeSync = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CMBlanguage = new System.Windows.Forms.ComboBox();
+            this.CKIsMutiSub = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // BtnSelectSub
+            // 
+            this.BtnSelectSub.Location = new System.Drawing.Point(12, 12);
+            this.BtnSelectSub.Name = "BtnSelectSub";
+            this.BtnSelectSub.Size = new System.Drawing.Size(94, 29);
+            this.BtnSelectSub.TabIndex = 0;
+            this.BtnSelectSub.Text = "选择字幕";
+            this.BtnSelectSub.UseVisualStyleBackColor = true;
+            this.BtnSelectSub.Click += new System.EventHandler(this.BtnSelectSub_Click);
+            // 
+            // BtnSelectVideo
+            // 
+            this.BtnSelectVideo.Location = new System.Drawing.Point(12, 47);
+            this.BtnSelectVideo.Name = "BtnSelectVideo";
+            this.BtnSelectVideo.Size = new System.Drawing.Size(94, 29);
+            this.BtnSelectVideo.TabIndex = 1;
+            this.BtnSelectVideo.Text = "选择文件";
+            this.BtnSelectVideo.UseVisualStyleBackColor = true;
+            this.BtnSelectVideo.Click += new System.EventHandler(this.BtnSelectVideo_Click);
+            // 
+            // LBFiles
+            // 
+            this.LBFiles.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.LBFiles.FormattingEnabled = true;
+            this.LBFiles.ItemHeight = 20;
+            this.LBFiles.Location = new System.Drawing.Point(0, 86);
+            this.LBFiles.Name = "LBFiles";
+            this.LBFiles.Size = new System.Drawing.Size(800, 364);
+            this.LBFiles.TabIndex = 4;
+            // 
+            // LBSubCount
+            // 
+            this.LBSubCount.AutoSize = true;
+            this.LBSubCount.Location = new System.Drawing.Point(123, 16);
+            this.LBSubCount.Name = "LBSubCount";
+            this.LBSubCount.Size = new System.Drawing.Size(0, 20);
+            this.LBSubCount.TabIndex = 5;
+            // 
+            // BtnSplit
+            // 
+            this.BtnSplit.Location = new System.Drawing.Point(112, 47);
+            this.BtnSplit.Name = "BtnSplit";
+            this.BtnSplit.Size = new System.Drawing.Size(94, 29);
+            this.BtnSplit.TabIndex = 6;
+            this.BtnSplit.Text = "分割";
+            this.BtnSplit.UseVisualStyleBackColor = true;
+            this.BtnSplit.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // BtnGetAudio
+            // 
+            this.BtnGetAudio.Location = new System.Drawing.Point(212, 47);
+            this.BtnGetAudio.Name = "BtnGetAudio";
+            this.BtnGetAudio.Size = new System.Drawing.Size(94, 29);
+            this.BtnGetAudio.TabIndex = 7;
+            this.BtnGetAudio.Text = "提取音频";
+            this.BtnGetAudio.UseVisualStyleBackColor = true;
+            this.BtnGetAudio.Click += new System.EventHandler(this.BtnGetAudio_ClickAsync);
+            // 
+            // TxtFileIndex
+            // 
+            this.TxtFileIndex.Location = new System.Drawing.Point(557, 49);
+            this.TxtFileIndex.Name = "TxtFileIndex";
+            this.TxtFileIndex.Size = new System.Drawing.Size(125, 27);
+            this.TxtFileIndex.TabIndex = 9;
+            this.TxtFileIndex.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(467, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "文件开始值";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(437, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "字幕时间轴偏移";
+            // 
+            // TxtSubTimeSync
+            // 
+            this.TxtSubTimeSync.Location = new System.Drawing.Point(557, 9);
+            this.TxtSubTimeSync.Name = "TxtSubTimeSync";
+            this.TxtSubTimeSync.Size = new System.Drawing.Size(125, 27);
+            this.TxtSubTimeSync.TabIndex = 11;
+            this.TxtSubTimeSync.Text = "0:00:00:000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(688, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "0:00:00:000";
+            // 
+            // CMBlanguage
+            // 
+            this.CMBlanguage.FormattingEnabled = true;
+            this.CMBlanguage.Location = new System.Drawing.Point(312, 49);
+            this.CMBlanguage.Name = "CMBlanguage";
+            this.CMBlanguage.Size = new System.Drawing.Size(130, 28);
+            this.CMBlanguage.TabIndex = 14;
+            // 
+            // CKIsMutiSub
+            // 
+            this.CKIsMutiSub.AutoSize = true;
+            this.CKIsMutiSub.Location = new System.Drawing.Point(312, 8);
+            this.CKIsMutiSub.Name = "CKIsMutiSub";
+            this.CKIsMutiSub.Size = new System.Drawing.Size(106, 24);
+            this.CKIsMutiSub.TabIndex = 15;
+            this.CKIsMutiSub.Text = "是否多语言";
+            this.CKIsMutiSub.UseVisualStyleBackColor = true;
+            this.CKIsMutiSub.CheckedChanged += new System.EventHandler(this.CKIsMutiSub_CheckedChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CKIsMutiSub);
+            this.Controls.Add(this.CMBlanguage);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtSubTimeSync);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtFileIndex);
+            this.Controls.Add(this.BtnGetAudio);
+            this.Controls.Add(this.BtnSplit);
+            this.Controls.Add(this.LBSubCount);
+            this.Controls.Add(this.LBFiles);
+            this.Controls.Add(this.BtnSelectVideo);
+            this.Controls.Add(this.BtnSelectSub);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "根据字幕分割音频";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button BtnSelectSub;
+        private Button BtnSelectVideo;
+        private ListBox LBFiles;
+        private Label LBSubCount;
+        private Button BtnSplit;
+        private Button BtnGetAudio;
+        private TextBox TxtFileIndex;
+        private Label label1;
+        private Label label2;
+        private TextBox TxtSubTimeSync;
+        private Label label3;
+        private ComboBox CMBlanguage;
+        private CheckBox CKIsMutiSub;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Xabe.FFmpeg;
+
+namespace 按字幕截取音频
+{
+    public partial class MainForm : Form
+    {
+        private Task LoadSubTask;
+        private bool IsMutiSub = false;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public class SubTimeLine
+        {
+            public string Language { get; set; }
+            public TimeSpan StartTime { get; set; }
+            public TimeSpan EndTime { get; set; }
+            public string SubStr { get; set; }
+        }
+
+        List<string> FirstList;
+        List<SubTimeLine> subTimeLines;
+        string VideoPath = string.Empty;
+        private void BtnSelectSub_Click(object sender, EventArgs e)
+        {
+            FirstList = new List<string>();
+            subTimeLines = new List<SubTimeLine>();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "ass字幕文件|*.ass|srt字幕文件|*.srt";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                if (openFileDialog.FileName.Contains(".ass"))
+                {
+                    LoadSubTask = Task.Factory.StartNew(() =>
+                    {
+                        SplitAss(openFileDialog.FileName);
+                    });
+                }
+                else if (openFileDialog.FileName.Contains(".srt"))
+                {
+                    LoadSubTask = Task.Factory.StartNew(() =>
+                    {
+                        SplitSrt(openFileDialog.FileName);
+                    });
+                }
+            }
+        }
+
+        private void SplitAss(string Path)
+        {
+            BeginInvoke(() =>
+            {
+                LBSubCount.Text = "载入中";
+            });
+            string[] ReadText = File.ReadAllLines(Path, Encoding.UTF8);
+            string SubF = "Dialogue:";
+            foreach (var item in ReadText)
+            {
+                if (item.ToUpper().Contains(SubF.ToUpper()))
+                {
+                    string TempItem = item.Replace(SubF, string.Empty);
+                    TempItem = TempItem.Replace("\\N", string.Empty);
+                    FirstList.Add(TempItem.Trim());
+                }
+            }
+
+            foreach (var item in FirstList)
+            {
+                string[] textSplit = item.Split(',');
+                if (IsMutiSub)
+                {
+                    if (!textSplit[3].ToUpper().Contains("JP"))
+                    {
+                        continue;
+                    }
+                }
+                string StartTime = textSplit[1].Contains(".") ? textSplit[1].Replace(".", ":") : textSplit[1].Replace(",", ":");
+                string EndTime = textSplit[2].Contains(".") ? textSplit[2].Replace(".", ":") : textSplit[2].Replace(",", ":");
+                SubTimeLine subTimeLine = new SubTimeLine
+                {
+                    Language = textSplit[3].Trim(),
+                    StartTime = ConvertToTimeSpan(StartTime.Trim()),
+                    EndTime = ConvertToTimeSpan(EndTime.Trim()),
+                    SubStr = textSplit[9]
+                };
+                subTimeLines.Add(subTimeLine);
+            }
+
+            if (subTimeLines.Count > 0 && IsMutiSub)
+            {
+                BeginInvoke(() =>
+                {
+                    CMBlanguage.Items.Clear();
+                    foreach (var item in subTimeLines.DistinctBy(i => i.Language).ToList())
+                    {
+                        CMBlanguage.Items.Add(item.Language);
+                    }
+                });
+
+            }
+            BeginInvoke(() =>
+            {
+                LBSubCount.Text = "载入完成";
+            });
+        }
+
+        private void SplitSrt(string Path)
+        {
+            BeginInvoke(() =>
+            {
+                LBSubCount.Text = "载入中";
+            });
+            string[] ReadText = File.ReadAllLines(Path, Encoding.UTF8);
+            List<string> NewStrList = new List<string>();
+            foreach (var item in ReadText)
+            {
+                if (!string.IsNullOrEmpty(item))
+                {
+                    string TempStr = item.Replace("\r", string.Empty);
+                    TempStr = TempStr.Replace("\n", string.Empty);
+                    NewStrList.Add(TempStr);
+                }
+            }
+            bool IsFirst = true;
+            List<string> lineList = new List<string>();
+            List<string> TimeLineList = new List<string>();
+            foreach (var item in NewStrList)
+            {
+                if (item.All(char.IsDigit))
+                {
+                    if (IsFirst == false)
+                    {
+                        TimeLineList.Add(string.Join("|", lineList));
+                        lineList.Clear();
+                        IsFirst = true;
+                    }
+                    lineList.Add(item);
+                }
+                else
+                {
+                    IsFirst = false;
+                    lineList.Add(item);
+                }
+            }
+            if (lineList.Count > 0)
+            {
+                TimeLineList.Add(string.Join("|", lineList));
+                lineList.Clear();
+            }
+            foreach (var item in TimeLineList)
+            {
+                string[] items = item.Split('|');
+                string TimeLine = items[1].Replace("-->", "|");
+                string[] times = TimeLine.Split('|');
+                SubTimeLine subTimeLine = new SubTimeLine
+                {
+                    StartTime = ConvertToTimeSpan(times[0].Replace(",", ":").Trim()),
+                    EndTime = ConvertToTimeSpan(times[1].Replace(",", ":").Trim()),
+                    SubStr = items[2]
+                };
+                subTimeLines.Add(subTimeLine);
+
+            }
+
+            BeginInvoke(() =>
+            {
+                LBSubCount.Text = "载入完成";
+            });
+        }
+
+        private TimeSpan ConvertToTimeSpan(string text)
+        {
+            string cadena = text;
+            string[] partes = cadena.Split(new char[] { ':' }).Select(x => x.ToString()).ToArray();
+            string MiiSec = partes[3].ToString().PadRight(3, '0');
+            TimeSpan returnSpan = new TimeSpan(0, Convert.ToInt32(partes[0]), Convert.ToInt32(partes[1]), Convert.ToInt32(partes[2]), Convert.ToInt32(MiiSec));
+            return returnSpan;
+        }
+
+        private async Task<bool> SplitAudioAsync(string Path)
+        {
+            bool IsSucc = true;
+            int index = Convert.ToInt32(TxtFileIndex.Text);
+            try
+            {
+                IMediaInfo info = await FFmpeg.GetMediaInfo(Path);
+                if (IsMutiSub && !string.IsNullOrEmpty(CMBlanguage.Text))
+                {
+                    var ItemList = subTimeLines.Where(x => x.Language == CMBlanguage.Text);
+                    foreach (var item in ItemList)
+                    {
+                        index = index + 1;
+                        int FileIndex = index;
+                        TimeSpan timeSpan = ConvertToTimeSpan(TxtSubTimeSync.Text);
+                        TimeSpan NewStartTime = item.StartTime.Add(timeSpan);
+                        TimeSpan NewEndTime = item.EndTime.Add(timeSpan);
+                        TimeSpan duration = NewEndTime - NewStartTime;
+                        IAudioStream? audio = info.AudioStreams.FirstOrDefault()?.Split(NewStartTime, duration);
+                        string OutPath = Environment.CurrentDirectory.ToString() + "\\" + "SplitWavs\\" + FileIndex.ToString() + ".wav";
+                        await FFmpeg.Conversions.New()
+                  .AddStream(audio)
+                  .SetOutput(OutPath)
+                  .Start();
+                        LBFiles.Items.Add(OutPath);
+                        LBFiles.SelectedIndex = LBFiles.Items.Count - 1;
+                        string result1 = Environment.CurrentDirectory.ToString() + "\\" + "SplitWavs\\" + "list.txt";//
+                        FileStream fs = new FileStream(result1, FileMode.Append);
+                        StreamWriter wr = wr = new StreamWriter(fs);
+                        wr.WriteLine("wavs/" + FileIndex + ".wav" + "|" + item.SubStr.Trim());
+                        wr.Close();
+                    }
+                }
+                else
+                {
+                    foreach (var item in subTimeLines)
+                    {
+                        index = index + 1;
+                        int FileIndex = index;
+                        TimeSpan timeSpan = ConvertToTimeSpan(TxtSubTimeSync.Text);
+                        TimeSpan NewStartTime = item.StartTime.Add(timeSpan);
+                        TimeSpan NewEndTime = item.EndTime.Add(timeSpan);
+                        TimeSpan duration = NewEndTime - NewStartTime;
+                        IAudioStream? audio = info.AudioStreams.FirstOrDefault()?.Split(NewStartTime, duration);
+                        string OutPath = Environment.CurrentDirectory.ToString() + "\\" + "SplitWavs\\" + FileIndex.ToString() + ".wav";
+                        await FFmpeg.Conversions.New()
+                  .AddStream(audio)
+                  .SetOutput(OutPath)
+                  .Start();
+                        LBFiles.Items.Add(OutPath);
+                        LBFiles.SelectedIndex = LBFiles.Items.Count - 1;
+                        string result1 = Environment.CurrentDirectory.ToString() + "\\" + "SplitWavs\\" + "list.txt";//
+                        FileStream fs = new FileStream(result1, FileMode.Append);
+                        StreamWriter wr = wr = new StreamWriter(fs);
+                        wr.WriteLine("wavs/" + FileIndex + ".wav" + "|" + item.SubStr.Trim());
+                        wr.Close();
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                LBFiles.Items.Add(index + ":" + ex.Message);
+                MessageBox.Show(ex.Message);
+                IsSucc = false;
+            }
+            return IsSucc;
+        }
+
+        private void BtnSelectVideo_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MKV视频文件|*.mkv|MP4视频文件|*.mp4|WAV音频|*.wav";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                VideoPath = openFileDialog.FileName;
+            }
+        }
+
+        private async void BtnStart_Click(object sender, EventArgs e)
+        {
+            if (subTimeLines.Count > 0)
+            {
+                if (!LoadSubTask.IsCompleted)
+                {
+                    MessageBox.Show("请等待字幕载入完毕!");
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("请载入字幕!");
+                return;
+            }
+            if (string.IsNullOrEmpty(VideoPath))
+            {
+                return;
+            }
+            if (MessageBox.Show("是否开始分割?", "", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                if (await SplitAudioAsync(VideoPath))
+                {
+                    MessageBox.Show("分割成功!");
+                }
+                else
+                {
+                    MessageBox.Show("分割失败!");
+                }
+            }
+        }
+
+        private async void BtnGetAudio_ClickAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(VideoPath))
+                {
+                    return;
+                }
+                IMediaInfo info = await FFmpeg.GetMediaInfo(VideoPath);
+                IAudioStream? audio = info.AudioStreams.FirstOrDefault();
+                string OutPath = Environment.CurrentDirectory.ToString() + "\\" + "wavs\\" + "audio" + ".wav";
+                await FFmpeg.Conversions.New()
+          .AddStream(audio)
+          .SetOutput(OutPath)
+          .Start();
+                LBFiles.Items.Add(OutPath);
+                LBFiles.SelectedIndex = LBFiles.Items.Count - 1;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("提取错误");
+            }
+        }
+
+        private void CKIsMutiSub_CheckedChanged(object sender, EventArgs e)
+        {
+            IsMutiSub = CKIsMutiSub.Checked;
+        }
+    }
+}
